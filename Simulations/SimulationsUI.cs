de12c120dfb1ec2d50e9be7ea9c3c45dfbcc5113
guth.jarkovsky.tomas@gmail.com <guth.jarkovsky.tomas@gmail.com>
2022-08-14 02:12:00 +0200

    UI upgrade

--- Simulations/SimulationsUI.cs
+++ Simulations/SimulationsUI.cs
@@ -9,7 +9,12 @@ namespace DeepCrawlSims.Simulations
 {
     class SimulationsUI
     {
+        const int RUN_CONST = 3;
+
         BattleManager battleManager;
+        int command;
+        Party currentParty;
+        Creature currentCreature;
 
         public SimulationsUI(BattleManager battleManager)
         {
@@ -18,6 +23,23 @@ namespace DeepCrawlSims.Simulations
 
         public void Run()
         {
+            while (command != 0)
+            {
+                switch (command)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 1:
+                        break;
+                    case 1:
+                        break;
+                    case 1:
+                        break;
+                    default:
+                }
+            }
             ShowPartyToConsole(battleManager.allyParty);
             ShowPartyToConsole(battleManager.enemyParty);
 
@@ -25,6 +47,7 @@ namespace DeepCrawlSims.Simulations
 
         public void ShowPartyToConsole(Party party)
         {
+            Console.Clear();
             foreach (var creature in party.Creatures)
             {
                 Console.WriteLine();
@@ -34,8 +57,63 @@ namespace DeepCrawlSims.Simulations
                 string description = string.Join("\n",creature.ProcessQuery(query).descs);
                 Console.WriteLine(description);
             }
+
+            Console.WriteLine("What do you want to do now? (enter apropriate number)");
+            Console.WriteLine("1-4 = Modify party member");
+            Console.WriteLine("5 = Go back");
+
+            int input = 0;
+            while (input == 0)
+            {
+                input = Console.ReadLine()[0] - '0';
+            }
+
+            if (input >0 && input <5)
+            {
+                ShowUpgradesToConsole(party.Creatures[input - 1]);
+            }
             
         }
+        public void ShowMainMenuToConsole(Party party)
+        {
+            Console.Clear();
+            Console.WriteLine("What do you want to do now? (enter apropriate number)");
+            Console.WriteLine("1 = Show/modify first party");
+            Console.WriteLine("2 = Show/show second party");
+            Console.WriteLine("3 = Modify number of simulations");
+            Console.WriteLine("4 = Run simulations");
 
+            int input=0;
+            while (input == 0)
+            {
+                input = Console.ReadLine()[0] -'0';
+            }
+
+            switch (input)
+            {
+                case 1:
+                    ShowPartyToConsole(battleManager.allyParty);
+                    break;
+                case 2:
+                    ShowPartyToConsole(battleManager.enemyParty);
+                    break;
+                case 3:
+                    ShowPartyToConsole(battleManager.allyParty); //TODO
+                    break;
+                case 4:
+                    command = RUN_CONST;
+                    break;
+            }
+
+
+        }
+
+        public void ShowUpgradesToConsole(Creature creature)
+        {
+            foreach (var item in creature.components)
+            {
+                if 
+            }
+        }
     }
 }
